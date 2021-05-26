58d78848fe660586a46bde49fd1de589ac8f2b3b
Raphaël <raphgeorget.kid@sfr.fr>
2021-05-26 23:26:53 +0200

    V 3.0

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 """
 Alessio CORTESE
-Ethan
+Ethan TILLIER
 Lucas PETOVELLO
 Raphaël GEORGET
 """
-# ------------------------------------------------Imports---------------------------------------------------------------
+# --------------------------------------------------Imports-------------------------------------------------------------
 from tkinter import *
 from functools import partial
-from threading import Thread
+
 import re
 import random
 
 # ---------------------------------------------Variables globales-------------------------------------------------------
 Largeur = 1280  # Largeur CANVAS
 Hauteur = 720  # Hauteur CANVAS
-Score = 0  # Score du jeu
-meilleur_score = 0  # Variable de meilleur scores
 
 
 # °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
@@ -25,12 +23,13 @@ meilleur_score = 0  # Variable de meilleur scores
 def FenFormulaire():
     """
     Crée la Fenetre d'enregistrement des scores
+    Partial est une variante plus simple à utiliser que lambda et plus facilement lisible
     """
-    global fenetre, Score
+    global fenetre
     Pop_up = Toplevel(fenetre)
     Pop_up.geometry("400x200")
     Pop_up.title('Enregistrer')
-    Instructions = Label(Pop_up, text=f"Votre score actuel est {Score}")
+    Instructions = Label(Pop_up, text=f"Votre score actuel est {Jeu.Score}")
     Instructions.grid(columnspan="2", row="0")
     Formulaire = Entry(Pop_up)
     Formulaire.grid(columnspan="2", row="1")
@@ -40,17 +39,16 @@ def FenFormulaire():
     Annuler.grid(column="0", row="2")
 
 
+# command = lambda : f(1,2,3)
 # Sauvegarde des Scores :
 def Sauv(Formulaire, Pop_up):
     """
     Permet d'enregistrer les données dans le document annexe
     :param Formulaire: Variable contenant la donnée saisie par l'utilisateur
     :param Pop_up: Variable contenant la fenetre
-    :return: Nothing
     """
-    global Score
     nom = Formulaire.get()
-    score = Score
+    score = Jeu.Score
     fichier_score = open("scores.dat", "a")
     fichier_score.write(f"\n{nom}\n{score}")
     fichier_score.close()
@@ -59,6 +57,9 @@ def Sauv(Formulaire, Pop_up):
 
 # Lecture des meilleurs scores
 def Records():
+    """
+    Enregistre le score dans le document scores.dat
+    """
     Nouvelle_fen = Toplevel(fenetre)
     Nouvelle_fen.geometry("500x700")
     Nouvelle_fen.resizable(width=False, height=False)
@@ -101,72 +102,46 @@ fenetre.iconphoto(False, main_icon)
 
 # -------------------------------------------------Début du Flappy------------------------------------------------------
 jeu = Canvas(fenetre, bg="#4EC0CA", width=Largeur, height=Hauteur)
-jeu.pack()
+jeu.pack()  # Alternative Tkinter à la fonction Grid, très utile ici car la fenetre contient uniquement le Canevas
 
 
 class Oiseau:
     """
     Classe qui regroupe les diférents paramètres de l'oiseau
     """
-    Pause = True
-    img = PhotoImage(file="bird.gif")
+
+    img = PhotoImage(file="bird.gif")  # Permet d'enregistrer une image dans une variable. ici celle de l'oiseau
 
     def __init__(self):
         print("Création de l'oiseau")
-        self.posX = Largeur / 6.5
-        self.posY = Hauteur / 2
+        self.posX = Largeur / 6.5  # Positionne l'oiseau sur le canevas en fonction de sa taille ici la Largeur
+        self.posY = Hauteur / 2  # Positionne l'oiseau sur le canevas en fonction de sa taille ici la Hauteur
         self.image = jeu.create_image(self.posX, self.posY, image=Oiseau.img)
-        self.velocity = 1
+        self.coordsOiseau = jeu.bbox(self.image)  # Enregistre X1 X2 et le Y1 Y2 de l'image OISEAU
+        self.velocity = 1  # Velocity = Vitesse en anglais
         self.multiplicateur = 1.0  # Simulation d'une gravitée
-        self.framerate = 10  # Fréquence de raffraichissement 10ms
-        self.saut = False
-        self.Perdu = False
-
-    def lancer(self):
-        Oiseau.Pause = False
-
-    lancement = classmethod(lancer)
-
-    def Hitbox(self):
-        """
-
-        """
-        if self.posY >= Hauteur - 30:
-            print('Tombé !')
-            self.multiplicateur = 1
-            self.posY = Hauteur - 30
-            self.Perdu = True
-        if self.posY <= 42:
-            self.posY = 41
-
 
     def Gravite(self):
         """
         Moteur de l'oiseau, cette fonction "infinnie" regroupe la gravitée et fait appel aux fonction Hitboxs et Saut
         """
-        if not self.Perdu:
-            self.Hitbox()
-            jeu.bind("<Button-1>", self.Saut)
-            if self.multiplicateur < 6:
-                self.multiplicateur += 0.05
-            self.posY += self.velocity * self.multiplicateur
-            jeu.coords(self.image, self.posX, self.posY)
-            fenetre.after(self.framerate, self.Gravite)
-        else:
-            print('Perdu !')
+        if self.multiplicateur < 6:
+            self.multiplicateur += 0.05
+        self.posY += self.velocity * self.multiplicateur
+        jeu.coords(self.image, self.posX, self.posY)
 
     def Saut(self, event=None):
         """
+        event=None, Obligatoire car contenant self en premier param et étant actioné par un BIND
         Fait Sauter L'oiseau
         """
-        self.multiplicateur = -3.2
+        self.multiplicateur = -3
 
 
 class Tuyaux:
     """
     Classe qui regroupe les diférents paramètres des tuyaux
     """
-    Pause = True
 
     def __init__(self):
         self.AX1 = 1100
@@ -180,13 +155,10 @@ class Tuyaux:
         self.Tuyau_haut = jeu.create_rectangle(self.AX1, self.AY1, self.AX2, self.AY2, fill="green", outline="red")
         self.Tuyau_bas = jeu.create_rectangle(self.BX1, self.BY1, self.BX2, self.BY2, fill="green", outline="red")
 
-    def lancer(cls):
-        Tuyaux.Pause = False
-
     def GenerationTuyaux(self):
-        aleatoire = random.randint(50, Hauteur - 170)
+        aleatoire = random.randint(50, Hauteur - 185)
         self.AY2 = aleatoire
-        self.BY1 = aleatoire + 170
+        self.BY1 = aleatoire + 185
 
     def MouvementTuyaux(self):
         self.AX1 -= 1
@@ -195,46 +167,117 @@ class Tuyaux:
         self.BX1 -= 1
         self.BX2 -= 1
         jeu.coords(self.Tuyau_bas, self.BX1, self.BY1, self.BX2, self.BY2)
-        fenetre.after(5, self.MouvementTuyaux)
+        if self.AX1 == 0:
+            self.AX1 = 1100
+            self.AY1 = 0
+            self.AX2 = 1000
+            self.BX1 = 1100
+            self.BX2 = 1000
+            self.BY2 = Hauteur
+            self.GenerationTuyaux()
+
+        # print(Tuyaux.Perdu)
 
 
-#  ------------------------------------Programmation Externe du Flappy Bird---------------------------------------------
+class Jeu(Oiseau, Tuyaux):
+    """
+    Classe maitresse qui utilise les données des classe Tuyaux et Oiseau afin de créer le jeu
+    Cette classe s'initialise avec en mémoire l'ensemble des fonctions et varaibles __init() des classes Oiseau Tuyaux
+    ce qui permet le d'accèder, de modifier, de lire ce que l'on veut des deux classes.
+    Pour résumer, nous avons vu cette année les fonctions avec le mot clé : "global ..." qui permetait de récupérer des
+    variables exterieures. Ici pour des raisons de simplifications et de lisibilités de ce code nous avons choisit les
+    classes
+    L’héritage dans la programmation permet de faire plus avec moins de code et de répétition.
+    l'héritage garantie l’efficacité et la clarté du code.
+    """
+    Score = 0
+
+    def __init__(self):
+        self.pause = False  # Comme son nom l'indique ^^
+        self.perdu = False  # Comme son nom l'indique ^^
+        self.Latence_Oiseau = 10  # latence en milisecondes de l'oiseau, Correspond au délai avant la prochaine --->
+        self.Latence_Tuyaux = 4  # éxécution. Se rapproche d'une boucle infinnie. VARIABLE UTILISEE PLUS TARD
+        Oiseau.__init__(self)  # Récupère les variables globales de la classe Oiseau comme ses coordonnées etc ...
+        Tuyaux.__init__(self)  # Récupère les variables globales de la classe Tuyaux comme ses coordonnées etc ...
+
+    def Lancement_Jeu(self):
+        if not self.pause and not self.perdu:
+            self.Boucle_Oiseau()
+            self.Boucle_Tuyaux()
+            jeu.bind("<Button-1>", self.Saut)
+        else:
+            print('Jeu déja lancé !')
+
+    def Boucle_Oiseau(self):
+        if not self.perdu:
+            self.Gravite()
+            self.Verif_Hitbox()
+            self.coordsOiseau = jeu.bbox(self.image)  # Met à jour la variable
+            jeu.after(self.Latence_Oiseau, self.Boucle_Oiseau)
+
+    def Boucle_Tuyaux(self):
+        if not self.perdu:
+            self.MouvementTuyaux()
+            if self.AX1 == 0:
+                Jeu.Score += 1
+            jeu.after(self.Latence_Tuyaux, self.Boucle_Tuyaux)
+
+    def Verif_Hitbox(self):
+        """
+        Canalise l'oiseau dans la fenêtre
+        Arrête le jeu en cas de contact avec un Tuyau
+        """
+        # Arrete le jeu si l'oiseau touche le sol
+        if self.posY >= Hauteur - 30:
+            self.multiplicateur = 1
+            self.posY = Hauteur - 30
+            self.perdu = True
+        # Empeche l'oiseau de sortir vers le haut
+        if self.posY <= 42:
+            self.posY = 41
+        # Arrete le jeu si collision
+        Haut = jeu.find_overlapping(self.AX1, self.AY1, self.AX2, self.AY2)
+        Bas = jeu.find_overlapping(self.BX1, self.BY1, self.BX2, self.BY2)
+        if len(Haut) == 2 or len(Bas) == 2:
+            self.perdu = True
+
+
+#  --------------------------------------------Programmation Externe du Flappy Bird-------------------------------------
 def Jouer():
     """
     Démarre le Jeu
-    :return:
+    Empeche le jeu de se relancer. find.all renvoie une liste d'ID présent, si > 0, le jeu à déjà été lancé
     """
-    if not Oiseau.Pause and not Tuyaux.Pause:
-        print('Jeu déja lancé !')
-    else:
-        lancement_Oiseau = Oiseau()
-        lancement_Tuyaux = Tuyaux()
-        lancement_Oiseau.lancer()
-        lancement_Tuyaux.lancer()
-        lancement_Oiseau.Gravite()
-        lancement_Oiseau.Hitbox()
-        lancement_Tuyaux.MouvementTuyaux()
+    if not len(jeu.find_all()) > 0:
+        lancer = Jeu()
+        lancer.Lancement_Jeu()
 
 
 def Changediff():
     """
     Ouvre le fichier de Configuration du jeu afin de modifer la Velocity (Vitesse)
     Détail : Boite de dialogue avec Entry puis Button en dessous
-    :return:
-    """
-    ...
-
-def Relancer():
-    """
-    Relance le jeu
+     /!\ Disponible dans la prochaine mise à jour, dans une nouvelle classe esclave de Jeu() dédiée /!\
     """
     pass
 
-# ---------------------------------------------Création du Menu---------------------------------------------------------
+
+def Relancer():
+    """
+    Supprime toutes les ID/ entitées du Canevas jeu
+    Réinitialise le Score
+    Relance le jeu
+    """
+    jeu.delete(ALL)
+    Jeu.Score = 0
+    Jouer()
+
+
+# ---------------------------------------------------Création du Menu---------------------------------------------------
 menu_bar = Menu(fenetre)
 
 file_menu = Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="Quitter", command=fenetre.quit)
+file_menu.add_command(label="Quitter", command=fenetre.destroy)
 menu_bar.add_cascade(label="Fichier", menu=file_menu)
 
 game_menu = Menu(menu_bar, tearoff=0)
@@ -253,4 +296,4 @@ fenetre.config(menu=menu_bar)
 # **********************************************************************************************************************
 # --------------------------------------------------FIN PROGRAMME-------------------------------------------------------
 # **********************************************************************************************************************
-fenetre.mainloop()
+fenetre.mainloop()  # Boucle de répétition
